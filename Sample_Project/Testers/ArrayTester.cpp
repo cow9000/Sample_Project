39e9bf90ce920f73dfd03bb6590b163a82c8fd6b
Derek Vawdrey <flyingpiechicken@gmail.com>
2018-02-06 13:03:43 -0700

    Added testAdvancedArray

--- Sample_Project/Testers/ArrayTester.cpp
+++ Sample_Project/Testers/ArrayTester.cpp
@@ -32,4 +32,30 @@ void ArrayTester:: testArrayUse(){
     thirdArray[5] = 123;
     std::cout << thirdArray[5] << std::endl;
     
+    
+    
+    
+}
+
+void ArrayTester::testAdvancedArray(){
+    vector<CrimeData> test = FileController::readCrimeDataToVector("/Users/dvaw7563/Documents/C++/Sample_Project/Sample_Project/Data/crime.csv");
+    int arraySize = test.size();
+    Array<CrimeData> data(arraySize);
+    
+    for(int i = 0; i < arraySize; i++){
+        data[i] = test[i];
+    }
+    
+    Timer vectorTimer;
+    Timer arrayTimer;
+    
+    vectorTimer.startTimer();
+    std::cout << test[424] << std::endl;
+    vectorTimer.stopTimer();
+    vectorTimer.displayInformation();
+    
+    arrayTimer.startTimer();
+    std::cout << data[424] << std::endl;
+    arrayTimer.stopTimer();
+    arrayTimer.displayInformation();
 }
