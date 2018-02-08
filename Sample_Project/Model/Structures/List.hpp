8c61510eda89eeee29faf68c2e6b497369bd3989
Derek Vawdrey <flyingpiechicken@gmail.com>
2018-02-08 11:35:56 -0700

    Added List.hpp

---
+++ Sample_Project/Model/Structures/List.hpp
@@ -0,0 +1,30 @@
+//
+//  List.hpp
+//  Sample_Project
+//
+//  Created by Vawdrey, Derek on 2/8/18.
+//  Copyright © 2018 Vawdrey, Derek. All rights reserved.
+//
+
+#ifndef List_hpp
+#define List_hpp
+
+#include <assert.h>
+#include "../../Nodes/LinearNode.hpp"
+
+class List{
+public:
+    virtual void add(Type item) = 0;
+    virtual void addAtIndex(int index, Type item) = 0;
+    virtual Type remove(Type item) = 0;
+    virtual Type getFromIndex(int index) = 0;
+    
+    virtual int getSize() const = 0;
+    virtual LinearNode<Type> * getFront() = 0;
+    virtual LinearNode<Type> * getEnd() = 0;
+protected:
+    int size;
+};
+
+
+#endif /* List_h */
