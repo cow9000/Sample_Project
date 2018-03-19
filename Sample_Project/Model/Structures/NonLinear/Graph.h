e20acbcc0d38454aa1b7a66c49a0ce88b3e16c06
Derek Vawdrey <flyingpiechicken@gmail.com>
2018-03-19 13:02:04 -0600

    Removal methods

--- Sample_Project/Model/Structures/NonLinear/Graph.h
+++ Sample_Project/Model/Structures/NonLinear/Graph.h
@@ -78,6 +78,39 @@ Type graph<Type> :: operator[](int vertex) const{
     return graphData[vertex];
 }
 
+template <class Type>
+void Graph<Type> :: addVertex(const Type& value){
+    assert(vertexCount < MAXIMUM);
+    int newVertexNumber = vertexCount;
+    vertexCount++;
+    
+    for(int otherVertexNumber = 0; otherVertexNumber < vertexCount; otherVertexNumber++){
+        adjacencyMatrix[otherVertexNumber][newVertexNumber] = false;
+        adjacencyMatrix[otherVertexNumber][otherVertexNumber] = false;
+    }
+    
+    graphData[newVertexNumber] = value;
+}
+
+template <class Type>
+void Graph<Type> :: removedEdge(int source, int target){
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    adjacenccyMatrix[source][target] = false;
+}
+
+template <class Type>
+void Graph<Type> :: removedEdgeUndirected(int source, int target){
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    adjacencyMatrix[source][target] = false;
+    adjacencyMatrix[target][source] = false;
+    
+}
+
+template <class Type>
+void Graph<Type> :: removeEdgeCost(int source, int target){
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    weightCostMatrix[source][target] = 0;
+}
 
 
 #endif /* Graph_hpp */
