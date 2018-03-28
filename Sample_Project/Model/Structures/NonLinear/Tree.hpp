6acf5bff2bf586f5e4cdc9c88f9f9cfb1f1691ec
Derek Vawdrey <flyingpiechicken@gmail.com>
2018-03-28 11:47:15 -0600

    Added tree.hpp

---
+++ Sample_Project/Model/Structures/NonLinear/Tree.hpp
@@ -0,0 +1,33 @@
+//
+//  Tree.hpp
+//  Sample_Project
+//
+//  Created by Vawdrey, Derek on 3/27/18.
+//  Copyright © 2018 Vawdrey, Derek. All rights reserved.
+//
+
+#ifndef Tree_hpp
+#define Tree_hpp
+
+#include "../../Nodes/BinaryTreeNode.hpp"
+
+template <class Type>
+class Tree{
+protected:
+    BinaryTreeNode<Type> * root;
+public:
+    virtual int getSize() = 0;
+    virtual int getHeight() = 0;
+    virtual bool isComplete() = 0;
+    virtual bool isBlanaced() = 0;
+    
+    virtual void insert(Type data) = 0;
+    virtual bool contains(Type data) = 0;
+    virtual void remove(Type data) = 0;
+    
+    virtual void inOrderTraversal() = 0;
+    virtual void preOrderTraversal() = 0;
+    virtual void postOrderTraversal() = 0;
+};
+
+#endif /* Tree_hpp */
