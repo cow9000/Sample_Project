fca6e498349d9390818002638089b3a5b65501df
Derek Vawdrey <flyingpiechicken@gmail.com>
2018-04-17 12:38:38 -0600

    added removeNode

--- Sample_Project/Model/Structures/NonLinear/AVLTree.hpp
+++ Sample_Project/Model/Structures/NonLinear/AVLTree.hpp
@@ -32,6 +32,11 @@ public:
     void remove(Type value);
 };
 
+template<class Type>
+AVLTree<Type> :: AVLTree() : BinarySearchTree<Type>(){
+    this->root = nullptr;
+}
+
 template <class Type>
 int AVLTree<Type> :: heightDifference(BinaryTreeNode<Type> * node){
     int balance;
@@ -97,6 +102,41 @@ BinaryTreeNode<Type> * AVLTree<Type> :: balanceSubTree(BinaryTreeNode<Type> * pa
     return parent;
 }
 
+
+template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: removeNode(BinaryTreeNode<Type> * parent, Type inserted){
+    if(parent == nullptr){
+        return parent;
+    }
+    
+    if(inserted < parent->getNodeData()){
+        parent->setleftChild(removeNode(parent->getLeftNode,inserted));
+        
+    }
+    else if(inserted > parent->getNodeData()){
+        parent->setRightChild(removeNode(parent->getRightNode(), inserted));
+    }else{
+        BinaryTreeNode<Type> * temp;
+        if(parent->getLeftNode() == nullptr && parent->getRightNode == nullptr){
+            temp = parent;
+            delete temp;
+        }else if(parent->getLeftNode() == nullptr){
+            *parent = *parent->getRightNode();
+        }else if(parent->getRightNode() == nullptr){
+            *parent = *parent->getLeftNode();
+        }
+        else{
+            BinaryTreeNode<Type> * leftMost = this->getLeftMostChild(parent->getRightNode());
+            parent->setNodeData(leftMost->getNodeData());
+            parent->setRightChild(removeNode(parent->getRightNode(), leftMost->getNodeData()));
+        }
+    }
+    
+    if(parent == nullptr){
+        return parent;
+    }
+    return balanceSubTree(parent);
+}
 template <class Type>
 void AVLTree<Type>::insert(Type item)
 {
