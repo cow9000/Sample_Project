6186fde98dd54426e1d8a331ea7434b68b910bcc
Derek Vawdrey <flyingpiechicken@gmail.com>
2018-04-17 12:10:28 -0600

    Added rotations to AVLTree

--- Sample_Project/Model/Structures/NonLinear/AVLTree.hpp
+++ Sample_Project/Model/Structures/NonLinear/AVLTree.hpp
@@ -36,7 +36,29 @@ template <class Type>
 int AVLTree<Type> :: heightDifference(BinaryTreeNode<Type> * node){
     int balance;
     
+    int leftHeight = this->calculateHeight(node->getLeftNode());
+    int rightHeight = this->calculateHeight(node->getRightNode());
+    balance = leftHeight - rightHeight;
+    
     return balance;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: leftRotation(BinaryTreeNode<Type> * parent){
+    BinaryTreeNode<Type> * changedNode;
+    changedNode = parent->getLeftNode();
+    parent->setLeftChild(changedNode->getRightNode());
+    changedNode->setRightChild(parent);
+    return changedNode;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: rightRotation(BinaryTreeNode<Type> * parent){
+    BinaryTreeNode<Type> * changedNode;
+    changedNode = parent->getRightNode();
+    parent->setRightChild(changedNode->getLeftNode());
+    changedNode->setLeftChild(parent);
+    return changedNode;
 }
 
 #endif /* AVLTree_hpp */
