ff3c89e9b24d2fe2f019c2ffa3dd51a087f9865d
Derek Vawdrey <flyingpiechicken@gmail.com>
2018-05-03 12:01:37 -0600

    Added collision and insert

--- Sample_Project/Model/Structures/NonLinear/Hashtable.hpp
+++ Sample_Project/Model/Structures/NonLinear/Hashtable.hpp
@@ -23,7 +23,7 @@ private:
     bool isPrime(long current);
     void resize();
     long findPosition(HashNode<Type>* insertedNode);
-    long handleCollision(HashNode<Type> * insertedNode, long index);
+    long handleCollision(long index);
     
 public:
     Hashtable();
@@ -53,17 +53,28 @@ bool Hashtable<Type>::isPrime(long current){
 
 template <class Type>
 long HashTable<Type> :: findPosition(HashNode<Type> * insert){
-    return -1;
+    long insertPosition = insert->getKey() % this->capacity;
+    return insertPosition;
 }
 
 template <class Type>
-long Hashtable<Type> :: handleCollision(Hashnode<Type> * current, long index){
+long Hashtable<Type> :: handleCollision(long currentPosition){
+    long shift = 17;
+    for(long position = currentPosition + shift; position!=currentPosition; position+=shift){
+        if(position>= capacity){
+            position = position%capacity;
+        }
+        
+        if(internalStorage[position] == nullptr){
+            return position;
+        }
+    }
     return -1;
 }
 
 template <class Type>
 long Hashtable<Type>::getSize(){
-    return -1;
+    return this->size;
 }
 
 template <class Type>
@@ -73,12 +84,47 @@ long Hashtable<Type>::getNextPrime(){
 
 template <class Type>
 void Hashtable<Type> ::resize(){
+    long updatedCapacity = getNextPrime();
+    HashNode<Type> * * tempStorage = new HashNode<Type> * [updatedCapacity];
+    std::fill_n(tempStorage, updatedCapacity, nullptr);
     
+    long oldCapacity = this->capacity;
+    this->capacity = updatedCapacity;
+    
+    for(long index = 0; index < oldCapacity; index++){
+        if(internalStorage[index] != nullptr){
+            HashNode<Type>* temp = internalStorage[index];
+            long position = findPosition(temp);
+            
+            if(tempStorage[position] == nullptr){
+                tempStorage[position] = temp;
+            }else{
+                long updatedPosition = handleCollision(position);
+                if(updatedPosition != -1){
+                    tempStorage[updatedPosition] = temp;
+                }
+            }
+        }
+    }
+    internalStorage = tempStorage;
 }
 
 template <class Type>
 void Hashtable<Type>::insert(Type value){
-    
+    this->size++;
+    if(((this->size * 1.000)/this->capacity) > this->loadFactor){
+        resize();
+    }
+    HashNode<Type> * temp = new HashNode<Type>(value);
+    long index = findPosition(temp);
+    if(internalStorage[index] == nullptr){
+        internalStorage[index] = templ
+    }else{
+        long updatedPosition = handleCollision(index);
+        if(updatedPosition != -1){
+            internal[updatedPosition] = temp;
+        }
+    }
 }
 
 
